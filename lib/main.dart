a5a67e80b82e35dbafb27a82642ce57aa2c48faf
yusmnn <yusmanqq@gmail.com>
2022-11-16 04:57:58 +0800

    update: readme

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "FIC flutter basic",
-      home: HomePage(),
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const HomePage(),
     );
   }
 }
